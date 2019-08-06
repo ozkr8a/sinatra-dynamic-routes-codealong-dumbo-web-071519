875dfbadfc95ad70ca3e389c4689a727156cfc73
Oscar <oscar_ochoac@yahoo.com>
2019-08-06 14:06:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
 
@@ -14,5 +15,22 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
-
+  get '/property/:id' do
+    @property = all_properties.select do |property|
+      property.id == params[:id]
+    end
+    #erb :
+  end
+    
+  get '/goodbye/:name' do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+  
+  get '/multiply/:number1/:number2' do
+    # binding.pry
+    @product = params[:number1].to_i * params[:number2].to_i
+    #erb :multiply
+    "#{@product}"
+  end
 end
